ec421a4659e95b5fee3ed3fd261ce49f3609b8a7
patelbiren24 <patelbiren56@gmail.com>
2021-04-27 14:25:17 -0500

    added some profile styling from bootstrap examples

--- angular-src/src/app/Components/dashboard/dashboard.component.ts
+++ angular-src/src/app/Components/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  constructor(
+    public authService: AuthService,
+    private flashMessages: FlashMessagesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.authService.getProfile().subscribe(
+      (profile) => {
+        this.user = (profile as any).user;
+      },
+      (err) => {
+        console.log(err);
+        return false;
+      }
+    );
   }
-
 }
